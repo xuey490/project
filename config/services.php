4c967b52acd0208dd7c0cdde24ee441c0ff326f9
xuey490 <xuey863toy@gmail.com>
2025-11-19 22:43:59 +0800

    这是一个奇怪的分支
    对于thinkorm不同的服务注册状态的尝试

--- config/services.php
+++ config/services.php
@@ -9,6 +9,8 @@ use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
 use Framework\Container\ContainerProviders;
 
+	use Framework\Factory\ThinkORMFactory;
+
 return function (ContainerConfigurator $configurator) {
     $services = $configurator->services();
 
@@ -30,6 +32,7 @@ return function (ContainerConfigurator $configurator) {
 	$databseConfig  = require BASE_PATH . '/config/database.php';
 
     // === 注册 ThinkORM 模型工厂服务 ===
+	/*
     $services
         ->set(\Framework\Utils\ThinkORMFactory::class)
 		->args([$databseConfig])
@@ -37,6 +40,10 @@ return function (ContainerConfigurator $configurator) {
 
     // 可以额外 alias
     $services->alias('orm', \Framework\Utils\ThinkORMFactory::class);
+	*/
+	
+
+	
 
     // ✅ 1. 自动加载应用 Provider
     $providerManager = new ContainerProviders();
@@ -51,5 +58,26 @@ return function (ContainerConfigurator $configurator) {
 
     // ✅ 3. 启动所有 Provider（boot）
     $providerManager->bootProviders($configurator);
+	
+    // 工厂服务
+	$services->set(ThinkORMFactory::class)
+		->args([
+			$databseConfig,
+			service('log'),        // SQL 日志
+			service('log'),        // 慢 SQL 日志
+			200
+		])
+		->public();
+
+	// DbManager 使用工厂创建
+	$services->set(\think\DbManager::class)
+		->factory([ service(ThinkORMFactory::class), 'create' ])
+		->public();
+
+	// 你框架里的统一 service id
+	$services->alias('thinkorm', \think\DbManager::class);
+	
+	
+	
 
 };
