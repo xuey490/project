50f52b02d34c4104ba68d47f995b153e0c99287b
xuey490 <xuey863toy@gmail.com>
2025-12-19 23:14:35 +0800

    更新注释

--- framework/DI/ContextBag.php
+++ framework/DI/ContextBag.php
@@ -2,6 +2,18 @@
 
 declare(strict_types=1);
 
+/**
+ * This file is part of FssPHP Framework.
+ *
+ * @link     https://github.com/xuey490/project
+ * @license  https://github.com/xuey490/project/blob/main/LICENSE
+ *
+ * @Filename: %filename%
+ * @Date: 2025-12-19
+ * @Developer: xuey863toy
+ * @Email: xuey863toy@gmail.com
+ */
+
 namespace Framework\DI;
 
 final class ContextBag
