8ebba6805fd391b28e0c44e641a260b722425c38
xuey490 <xuey863toy@gmail.com>
2026-01-05 21:16:53 +0800

    更新 CasbinRbac.php

--- framework/Security/CasbinRbac.php
+++ framework/Security/CasbinRbac.php
@@ -1,6 +1,20 @@
 <?php
 
-namespace App\Core\Auth;
+declare(strict_types=1);
+
+/**
+ * This file is part of FssPHP Framework.
+ *
+ * @link     https://github.com/xuey490/project
+ * @license  https://github.com/xuey490/project/blob/main/LICENSE
+ *
+ * @Filename: %filename%
+ * @Date: 2025-11-24
+ * @Developer: xuey863toy
+ * @Email: xuey863toy@gmail.com
+ */
+
+namespace Framework\Security;
 
 use Casbin\Enforcer;
 use Casbin\Model\Model;
