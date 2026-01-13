22004df1090741d6975ac786218f4d4185e187f8
xuey490 <xuey863toy@gmail.com>
2026-01-13 22:10:21 +0800

    更新 TpBelongsToTenant.php

--- framework/Basic/Traits/TpBelongsToTenant.php
+++ framework/Basic/Traits/TpBelongsToTenant.php
@@ -2,6 +2,18 @@
 
 declare(strict_types=1);
 
+/**
+ * This file is part of FssPHP Framework.
+ *
+ * @link     https://github.com/xuey490/project
+ * @license  https://github.com/xuey490/project/blob/main/LICENSE
+ *
+ * @Filename: %filename%
+ * @Date: 2026-1-9
+ * @Developer: xuey863toy
+ * @Email: xuey863toy@gmail.com
+ */
+
 namespace Framework\Basic\Traits;
 
 use Framework\Basic\Scopes\TpTenantScope;
